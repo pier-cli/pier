efdd4ec0770ef7ba7b98b2d15958ea3c3123790f
BenSchZA <bscholtz.bds@gmail.com>
2019-03-06 23:08:23 +0200

    Add toml dependancies

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 extern crate clap;
 use clap::App;
 
+use toml;
+use serde_derive::{Debug, Deserialize};
+
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
