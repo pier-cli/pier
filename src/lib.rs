9931f6d17ba93584f370423025c47bd79df112af
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-09 16:29:49 +0200

    build cool table format with lazy static & prettytable

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use prettytable::{cell, format, row, Table};
+use prettytable::{cell, row, Table};
 use snafu::{ensure, OptionExt, ResultExt};
 use std::fs;
 use std::{path::PathBuf, process::ExitStatus};
@@ -23,6 +23,27 @@ pub struct Pier {
     config: Config,
     pub path: PathBuf,
     verbose: bool,
+}
+
+#[macro_use]
+extern crate lazy_static;
+
+use prettytable::format::LineSeparator;
+use prettytable::format::LinePosition;
+use prettytable::format::FormatBuilder;
+use prettytable::format::TableFormat;
+
+lazy_static! {
+    static ref COOL_SEP: LineSeparator = LineSeparator::new('\u{2256}', '\u{2256}', '\u{2256}', '\u{2256}');
+
+    pub static ref COOL_FORMAT: TableFormat = FormatBuilder::new()
+      .column_separator('\u{22EE}')
+      .borders('\u{22EE}')
+      .separator(LinePosition::Title, *COOL_SEP)
+      .separator(LinePosition::Bottom, *COOL_SEP)
+      .separator(LinePosition::Top, *COOL_SEP)
+      .padding(1, 1)
+      .build();
 }
 
 impl Pier {
